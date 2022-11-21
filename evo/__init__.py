aac0c0c4538bd7eefa8a2991df46fdd03e1d784c
Pip Liggins <pkl28@cam.ac.uk>
2022-11-21 18:20:53 +0000

    Added init and .toml file for local pip-install.

---
+++ evo/__init__.py
@@ -0,0 +1,12 @@
+"""
+EVo
+
+A Python model for volcanic degassing, using the equilibrium constants and mass balance method.
+"""
+
+__version__ = "1.0.1"
+__author__ = 'Philippa Liggins'
+
+# ----------------- IMPORTS ----------------- #
+from evo.dgs import main
+from evo.multirun import multirun
