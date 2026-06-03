d9b8a2ea3f712b1ff0bc2a2f4e63edec858593ad
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2026-06-03 16:28:55 +0100

    misc fixes

--- src/evo/__init__.py
+++ src/evo/__init__.py
@@ -5,10 +5,7 @@ A Python model for volcanic degassing, using the equilibrium constants and mass 
 method.
 """
 
-# ----------------- IMPORTS ----------------- #
 from evo.dgs import run_evo
 from evo.multirun import multirun
 
-__version__ = "1.0.2"
-__author__ = "Philippa Liggins"
 __all__ = ["multirun", "run_evo"]
